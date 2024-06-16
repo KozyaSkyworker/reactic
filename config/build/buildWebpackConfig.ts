2bc1bf36939d3dcd185db53f71b3ec9d99f1733e
KozyaSkyworker <d_koz@inbox.ru>
2024-06-16 20:39:22 +0400

    add dev server, env variables via webpack

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -1,13 +1,13 @@
-import path from "path";
 import { buildLoaders } from "./buildLoaders";
 import { buildPlugins } from "./buildPlugins";
 import { buildResolvers } from "./buildResolvers";
 import { BuildOptions } from "./types/config";
 import webpack from 'webpack'
+import { buildDevServer } from "./buildDevServer";
 
 export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
 
-    const {mode, paths} = options
+    const {mode, paths, isDev} = options
 
     return {
         mode: mode,
@@ -21,7 +21,11 @@ export function buildWebpackConfig(options: BuildOptions): webpack.Configuration
           rules: buildLoaders(),
         },
         resolve: buildResolvers(),
-        plugins: buildPlugins(options)
-         
+        plugins: buildPlugins(options),
+
+        // зачем собственном в проде это
+        devtool: isDev ? 'inline-source-map' : undefined, // inline-source-map чтобы видеть где в каком файле всё сломалось
+        devServer: isDev ? buildDevServer(options) : undefined
+        
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 import { myFn } from './test';
 
 myFn(1);
+
+
+document.body.innerHTML = `<h1>1</h1>`
