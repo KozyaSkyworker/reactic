ac98706db5849bcba5fd47687ba48db71cc4e925
KozyaSkyworker <d_koz@inbox.ru>
2024-06-17 18:14:43 +0400

    add react, css scss loaders

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import { myFn } from './test';
-
-myFn(1);
-
-
-document.body.innerHTML = `<h1>1</h1>`
--- src/test.ts
+++
@@ -1,5 +0,0 @@
-export const myFn = (arg: number):string => {
-  console.log('123');
-
-  return "danila"
-};
